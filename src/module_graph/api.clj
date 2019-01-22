36fc2a19d3097922d75cd0fa28a8adbcca044e11
y2k <itwisterlx@gmail.com>
2019-01-22 21:44:10 +0300

    refactoring

--- src/module_graph/api.clj
+++ src/module_graph/api.clj
@@ -1,74 +1,47 @@
 (ns module-graph.api
-  (:import (org.jsoup Jsoup)))
+  (:import (org.jsoup Jsoup))
+  (:require [clojure.string :as s]))
 
-(defn parseDocument [html]
-  (defn f [xs] (map #(-> % (.parent) (.parent)) xs))
-  (-> (Jsoup/parse html)
-      (.select "h2[id^=model_]")
-      (f)))
+(->>
+ (parseDocument (slurp api-file))
+ (map
+  (fn [e]
+    (defn parseClassName [e]
+      (.text (.selectFirst e "h2.panel-title")))
+    (defn parseProperties [e]
+      (defn findLastTab [e] (.select e "table.table.table-striped"))
+      (defn parseProperty [e]
+        {:name (.text (.selectFirst e "td"))
+         :type (.text (second (.select e "td")))})
+      (let [last-tab (findLastTab e)]
+        (->> (.select last-tab "tbody > tr")
+             (mapv parseProperty))))
 
-(defn toItem2 [element] (str element))
-
-(defn getParams [element]
-  (let [table (.selectFirst element "tbody")]
-    (if (nil? table) nil
-        (->>
-         (.select table "tr")
-         (distinctby #(-> % (.child 0) (.text)))
-         (map toItem2)
-         (clojure.string/join "")))))
-
-(defn distinctby [f col]
-  (->> col
-       (group-by f)
-       (map (comp first second))))
-
-(defn toItem [element]
-  (let [className (toClassName element)
-        params (getParams element)]
-    (if (nil? params)
-      nil
-      (str "class "  className  "(\n" params + "\n)"))))
-
-(defn toClassName [element]
-  (defn getPath [e] (-> (.selectFirst e "h2.panel-title") (.text)))
-  (defn f [x] (clojure.string/replace x #"-(\w)" #(.toUpperCase (%1 1))))
-  (->> (.split (getPath element) "/")
-       (last)
-       f))
-
-(comment
-
-  (count (parseDocument (slurp api-file)))
-  (def element (first (parseDocument (slurp api-file))))
-  (.text element)
-  (getParams element)
-
-  (type table)
-  (def table (.selectFirst element "tbody"))
-
-  (def xs (vec (.select table "tr")))
-  (distinctby #(-> % (.child 0) (.text)) xs)
-  (-> (first xs) (.child 0))
-  ; (.child (first xs) 0)
-
-  (let [table (.selectFirst element "tbody")]
-    (if (nil? table) nil
-        (->>
-         (.select table "tr")
-         (distinctby #(-> % (.child 0) (.text)))
-         (map toItem2)
-         (clojure.string/join ""))))
-
-  ; (def path (getPath element))
-  ; (toClassName (getPath element))
-  ; (toItem element)
-
-  (->>
-   (slurp api-file)
-   (parseDocument)
-   (map toItem)
-   (first)
-   (println))
-
-  (comment))
+    {:class (parseClassName e)
+     :props (parseProperties e)}))
+ (map
+  (fn [x]
+    (defn fix-prop [p]
+      (assoc p
+             :fixed-name (s/replace (:name p) #"_(\w)" #(s/upper-case (% 1)))
+             :type (case (:type p)
+                     "bool" "Bool"
+                     "int" "Long"
+                     "string[]" "List<String>"
+                     "int[]" "List<Long>"
+                     "string" "String"
+                     "float" "Float"
+                     "float64" "Double"
+                     "array" "List<*>"
+                     (:type p))))
+    {:class (:class x)
+     :props (map fix-prop (:props x))}))
+ (map
+  (fn [x]
+    (defn format-props [props]
+      (->> props
+           (map #(str "\t@SerializedName(\"" (:name %) "\")\n\tval " (:fixed-name %) ": " (:type %)))
+           (s/join ",\n\n")))
+    (str "class " (:class x) "(\n" (format-props (:props x)) "\n)\n")))
+ (s/join "\n")
+ ((fn [x] (spit (str (System/getProperty "user.home") "/result.kt") x) nil)))
